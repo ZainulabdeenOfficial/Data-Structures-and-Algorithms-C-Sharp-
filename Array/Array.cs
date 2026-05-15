a56fcaf0b44593af37d64099bbacd060d511c698
M Zain Ul Abideen <zu4425@gmail.com>
2026-05-15 21:39:44 +0500

    find minum value code in array

--- Array/Array.cs
+++ Array/Array.cs
@@ -5,3 +5,18 @@ for (int i = 0;i < a.length;i++)
 {
     Console.WriteLine(a[i]);
 }
+
+// find minimum value
+			
+		int[] s = {2,3,4,5,8};
+		int min = s[0];
+		for (int i = 0;i<s.Length;i++)
+		{
+			 if ( s[i] < min)
+			 {
+				 min = i;
+		
+			}
+			}
+		
+		Console.WriteLine(min);
